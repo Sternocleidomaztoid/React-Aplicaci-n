3edea295a00e89f41d51a53fe2d7bd9d53694d43
Sternocleidomaztoid <zaracho.zg@gmail.com>
2020-11-25 07:15:50 -0300

    1 file modified

--- src/index.js
+++ src/index.js
@@ -4,4 +4,22 @@ import ReactDOM from "react-dom";
 //import App from "./App";
 
 
-//ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+
+
+
+
+
+//function App(){
+ // return(
+  //  <form >
+ //<p>white and blue</p>
+ //<label><input type="radio" value="1" name="0" unchecked="">right</input></label>
+ //<label><input type="radio" value="0" name="0" unchecked="">wrong</input></label>
+//</form>
+ // )
+//}
+
+//ReactDOM.render(<Preguntado />, document.getElementById("root"));
